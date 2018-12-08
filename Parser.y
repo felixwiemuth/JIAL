felixwiemuth/JIAL

commit c9667a0a790ea9202b1afd76dc4627968954bb2a

Allow any chars after end of task

Parser.y
@@ -36,8 +36,8 @@ id { L.Id $$ }
 %%
 
 Task :: { Task }
-Task : NormalCharList beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask { Task { prelude = $1, name = $4, elements = reverse $7 } }
-     | beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask { Task { prelude = "", name = $3, elements = reverse $6 } }
+Task : NormalCharList beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask NormalCharList { Task { prelude = $1, name = $4, elements = reverse $7, epilogue = $9 } }
+     | beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask NormalCharList { Task { prelude = "", name = $3, elements = reverse $6, epilogue = $8 } }
 
 TaskElemList : {- empty -} { [] }
              | TaskElemList TaskElem { $2:$1  }
@@ -105,7 +105,7 @@ Sp : sp          { $1 }
 parseError :: [L.Token] -> a
 parseError tokens = error $ "Parse error on tokens " ++ show tokens
 
-data Task = Task { prelude :: String, name :: String, elements :: [TaskElem] }
+data Task = Task { prelude :: String, name :: String, elements :: [TaskElem], epilogue :: String }
   deriving (Eq, Show)
 
 data TaskElem

LexerTest.hs
@@ -110,4 +110,6 @@ testlist = TestList [
   , tl "F1e" "/*Nothing*/\ntask A {//nothing}\n}" [NormalChar '\n', BeginTaskHeader, sp, Id "A", sp, BeginTaskBody, NormalChar '\n', EndTask]
   , tl "F1f" "task A{{{}}}" $ [BeginTaskHeader, sp, Id "A", BeginTaskBody, BeginAction] ++ mkN "{}" ++ [EndAction, EndTask]
   , tl "F1g" "task A{{{{}}}}" $ [BeginTaskHeader, sp, Id "A", BeginTaskBody, BeginAction] ++ mkN "{{}}" ++ [EndAction, EndTask]
+  , tl "F1h" "task A {} " [BeginTaskHeader, sp, Id "A", sp, BeginTaskBody, EndTask, NormalChar ' ']
+  , tl "F1i" "task A {}\n\n" $ [BeginTaskHeader, sp, Id "A", sp, BeginTaskBody, EndTask] ++ mkN "\n\n"
   ]

commit 1f4843e6223f01412a2e9dce1ed8cc19446bc57a

Fix bug introduced with c9667a0

Parser.y
@@ -36,8 +36,8 @@ id { L.Id $$ }
 %%
 
 Task :: { Task }
-Task : NormalCharList beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask NormalCharList { Task { prelude = $1, name = $4, elements = reverse $7, epilogue = $9 } }
-     | beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask NormalCharList { Task { prelude = "", name = $3, elements = reverse $6, epilogue = $8 } }
+Task : NormalCharList beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask MaybeNormalCharList { Task { prelude = $1, name = $4, elements = reverse $7, epilogue = $9 } }
+     | beginTaskHeader Sp id Sp beginTaskBody TaskElemList endTask MaybeNormalCharList { Task { prelude = "", name = $3, elements = reverse $6, epilogue = $8 } }
 
 TaskElemList : {- empty -} { [] }
              | TaskElemList TaskElem { $2:$1  }
@@ -58,6 +58,10 @@ NormalCharList_ :: { String }
 NormalCharList_ : normalChar { [$1] }
                 -- | beginString StringCharList endString {  } -- If want to deal with Strings here
                 | NormalCharList_ normalChar { $2:$1 }
+
+MaybeNormalCharList :: { String }
+MaybeNormalCharList : {- empty -} { "" }
+                    | NormalCharList { $1 }
 
 IAP :: { TaskElem }
 IAP : beginInput Input Action { IAP $2 $3 }